57fbc70b00d0e05593dc4f14922a4cbea635db91
Tamara Luque <tam.luque@gmail.com>
2020-08-24 22:14:07 -0400

    first card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Question from './components/Question';
 
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
+        <div className="contenido contenido-principal">
+          <Question />
+        </div>
       </header>
     </div>
   );
