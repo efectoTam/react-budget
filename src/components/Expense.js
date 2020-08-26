894ee94ec1944e521b02bd2002c09c57cf7e1a72
Tamara Luque <tam.luque@gmail.com>
2020-08-25 21:46:41 -0400

    showing expenses on the interface

---
+++ src/components/Expense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Expense = ({ expense }) => (
+  <li className="gastos">
+    <p>
+      {expense.name}
+      <span className="gasto">$ {expense.amount}</span>
+    </p>
+  </li>
+);
+
+export default Expense;
